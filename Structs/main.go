87330e3f9c27a10dab1e4a42350dcc16c3ad79ed
avu12 <algebra3eloadas@gmail.com>
2020-06-22 18:17:01 +0200

    Maps initial commit

--- Structs/main.go
+++ Structs/main.go
@@ -16,13 +16,13 @@ type person struct {
 func main() {
 
 	var thirdperson person
-	thirdpersonPointer := &thirdperson
+	//thirdpersonPointer := &thirdperson
 	thirdperson.firstName = "ezisalex"
 	thirdperson.lastName = "ands"
 	thirdperson.contactInfo.email = "a@gmail.com"
 	thirdperson.contactInfo.zipCode = 1111
 	thirdperson.print()
-	thirdpersonPointer.updateName("stupidname")
+	thirdperson.updateName("stupidname")
 	thirdperson.print()
 }
 
@@ -30,5 +30,5 @@ func (p person) print() {
 	fmt.Printf("%+v", p)
 }
 func (p *person) updateName(newFirstName string) {
-	(*p).firstName = newFirstName
+	(p).firstName = newFirstName
 }
