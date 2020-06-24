850048d455a340e3e350af0f470b1d22888cb177
avu12 <algebra3eloadas@gmail.com>
2020-06-24 16:39:45 +0200

    error handlign added to assigment3

--- Assigment3/main.go
+++ Assigment3/main.go
@@ -8,7 +8,11 @@ import (
 func main() {
 	if len(os.Args) > 0 {
 		filename := os.Args[1]
-		file, _ := os.Open(filename)
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Println("Error")
+			os.Exit(19)
+		}
 		bs := make([]byte, 999999)
 		file.Read(bs)
 		fmt.Println(string(bs))
